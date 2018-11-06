03a02d6c38c94a824d8b265b562eb7178337e00f
Blakjak88 <dave.armour@gmail.com>
2018-11-06 18:20:32 +0800

    Add files via upload

--- Week1Main.cpp
+++ Week1Main.cpp
@@ -2,7 +2,6 @@
 #include <armadillo>							// !!! Must go after mlpack/core.hpp   ref: email from Ryan Curtin. !!!
 #include <mlpack/methods/ann/layer/layer.hpp>
 #include <mlpack/methods/ann/ffn.hpp>
-#include <mlpack/methods/ann/loss_functions/cross_entropy_error.hpp>
 #include <mlpack/methods/ann/init_rules/glorot_init.hpp>   // For Xavier Iniialisation
 #include <mlpack/core/optimizers/sgd/sgd.hpp>
 #include <mlpack/core/optimizers/adam/adam.hpp>
@@ -25,7 +24,9 @@ void main()
 	//
 	// # Imports 1,080 training images and 120 testing images of dimension
 	// # 64x64x3 from .H5 databases and runs a Conv net replicating the 
-	// # DeepLearning.ai exercise model. 
+	// # DeepLearning.ai exercise model.
+	//
+	// # Requires HDF5 and H5CPP libraries.
 	//  
 	// # Note:  The Python model in the exercise results in a training
 	// #        accuracy of 94.1% and a test accuracy of 78.3%. Nothing
@@ -82,26 +83,39 @@ void main()
 	testSetX /= 255;
 
 	// The model architecture as per the course example is:
-	// (1)Conv2D->(2)ReLU->(3)MaxPool->(4)Conv2D->(5)ReLU->(6)MaxPool->(7)Flatten->(8)FullyConnected
+	// (1)Conv2D->(2)ReLU->(3)MaxPool->(4)Conv2D->(5)ReLU->(6)MaxPool->(7)(Flatten->)(8)FullyConnected->(9)LogSoftMax
 	//
-	// Since the course used 4x4 filters and 2x2 filters which are non-standard, I have changed these
-	// to 5x5 and 3x3 filters. This makes the arithmeic easier for the padding calculation.
-	// 
 	//     Input Dim    Description
 	//     ---------    -----------
-	// (1)  64x64x3   -- Apply 8 filters of size 4x4 with stride = 2 and "Same" padding (= 2)
+	// (1)  64x64x3   -- Apply 8 filters of size 4x4 with stride = 2 and "Same" padding (= 2 is closest equivalent)
 	// (2)  65x65x8   -- Standard ReLU layer
 	// (3)  65x65x8   -- Pooling layer of size 8x8 with stride = 8
-	// (4)  8x8x8     -- Apply 16 filters of size 2x2 with stride = 1 and "Same" padding (= 1)
+	// (4)  8x8x8     -- Apply 16 filters of size 2x2 with stride = 1 and "Same" padding (= 1 is closest equivalent)
 	// (5)  9x9x16    -- Standard ReLU layer
 	// (6)  9x9x16    -- Pooling layer of size 4x4 with stride = 4
-	// (7)  2x2x16    -- Fully connected layer -> output = 6.
-	// (10) 64x6      -- LogSoftMax layer
+	// (7)  2x2x16    -- Flatten (this is taken care of by the Linear layer input in MLPACK)
+	// (8)  64x6      -- Fully connected layer -> output = 6.
+	// (9)  6x6       -- LogSoftMax layer
 
 	FFN <NegativeLogLikelihood<>, XavierInitialization> model;
-	model.Add<Convolution<> >(3, 8, 5, 5, 1, 1, 2, 2, 64, 64);
+	model.Add<Convolution<> >(
+		3,		// Nbr of input layers. (e.g. an RGB image of 64x64x3 has 3 layers, where a grayscale image of 64x64x1 has 1 layer.
+		8,		// Nbr of output layers/maps.
+		5,		// Filter Width
+		5,		// Filter Height
+		1,		// Stride for Width
+		1,		// Stride for Height
+		2,		// Padding for Width
+		2,		// Padding for Heigth
+		64,	// Input Height
+		64);	// Output Height
 	model.Add<LeakyReLU<> >();
-	model.Add<MaxPooling<> >(8, 8, 8, 8, true);
+	model.Add<MaxPooling<> >(
+		8,		// Width of Pooling filter
+		8,		// Height of Pooling filter
+		8,		// Stride for Width
+		8,		// Stride for Height
+		true);// True = floor / False = ceil (used for output dimension formula to round up or down if non-integer result)
 	model.Add<Convolution<> >(8, 16, 3, 3, 1, 1, 1, 1, 8, 8);
 	model.Add<LeakyReLU<> >();
 	model.Add<MaxPooling<> >(4, 4, 4, 4, true);
@@ -114,7 +128,7 @@ void main()
 	// - batchsize     = 64
 	// - maxIterations = 10000
 	AdamUpdate adamUpdate(1e-8, 0.9, 0.999);
-	SGD<AdamUpdate> optimizer(0.009, 1, 1080, 1e-05, true, adamUpdate);
+	SGD<AdamUpdate> optimizer(0.009, 64, 1000, 1e-05, true, adamUpdate);
 
 	trainSetY += 1;  // NegativeLogLikelihood expects output in range [1, N]. Data was in range [0,N-1] previously.
 	testSetY += 1;   // NegativeLogLikelihood expects output in range [1, N]. Data was in range [0,N-1] previously.
@@ -123,7 +137,6 @@ void main()
 	{
 		model.Train(trainSetX, trainSetY, optimizer);
 		optimizer.ResetPolicy() = false;   // Keep the trained parameter settings from the previous epoch.
-		std::cout << "\n\n\nEpoch #" << epoch << "\n\n";
 
 		// Matrix to store the predictions on train and validation datasets.
 		arma::mat     yPredict;
@@ -140,11 +153,11 @@ void main()
 		yPredictLabels = arma::index_max(yPredict, 0) + 1;
 		double testAccuracy = arma::accu(yPredictLabels == arma::round(testSetY))*100.0 / (double)testSetX.n_cols;
 
-		std::cout << "Training Accuracy   = " << trainAccuracy << "%\n"
-			<< "Test Accuracy = " << testAccuracy << "%\n\n";
+		if (!epoch%5)
+		   std::cout << "Epoch: " << epoch << "\t" << "Training Accuracy   = " << trainAccuracy << "%"	<< "\tTest Accuracy = " << testAccuracy << "%\n";
 	}
 
-	data::Save("data/CNNSignsRGBModel.bin", "model", model, false);
+	data::Save("data/CNNSignsRGBModel.bin", "model", model, false);   // Save the trained model.
 
 	getchar();
 }
